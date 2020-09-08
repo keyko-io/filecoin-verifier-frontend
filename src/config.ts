46c810c8235ff737c35d7967c4117e1f0d5ce1fe
Jernej Pregelj <jernej.pregelj88@gmail.com>
2020-09-08 13:11:43 +0200

    remote testnet primary

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,6 @@
 export const config = {
     apiUri: process.env.REACT_APP_API_URI || 'http://localhost:4000',
     lotusNodes: [{
-        name: 'Localhost Testnet',
-        code: 1,
-        url: 'ws://localhost:1234/rpc/v0',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.9CmiCCCeDI85E1Jnl3j50cp9oDjThyakPhmvnQ9XAgs'
-    },{
-        name: 'Localhost Net',
-        code: 461,
-        url: 'ws://localhost:1234/rpc/v0',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.9CmiCCCeDI85E1Jnl3j50cp9oDjThyakPhmvnQ9XAgs'
-    },{
         name: 'Remote Testnet',
         code: 1,
         url: 'wss://beta-verify.filecoin.io/api/rpc/v0',
@@ -20,6 +10,16 @@ export const config = {
         code: 461,
         url: 'wss://beta-verify.filecoin.io/api/rpc/v0',
         token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiXX0.niP0Kugzl3DJiM2sHY7gDRcWBSmr3B1AyVie2Coc67E'
+    },{
+        name: 'Localhost Testnet',
+        code: 1,
+        url: 'ws://localhost:1234/rpc/v0',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.9CmiCCCeDI85E1Jnl3j50cp9oDjThyakPhmvnQ9XAgs'
+    },{
+        name: 'Localhost Net',
+        code: 461,
+        url: 'ws://localhost:1234/rpc/v0',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.9CmiCCCeDI85E1Jnl3j50cp9oDjThyakPhmvnQ9XAgs'
     }],
     datacapExt: [
         { value: "1000", name: "KiB" },
