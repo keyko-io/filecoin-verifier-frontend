1a7d4182787a394d11840adde394635a50b43f95
Iván <ivan.alberquilla@gmail.com>
2020-11-04 17:52:04 +0100

    var name app github

--- src/dist/config.js
+++ src/dist/config.js
@@ -1,31 +1,35 @@
-export const config = {
+"use strict";
+exports.__esModule = true;
+exports.config = void 0;
+exports.config = {
     apiUri: process.env.REACT_APP_API_URI || 'http://localhost:4000',
     apiToken: process.env.REACT_APP_API_TOKEN || '',
     verifiers: process.env.REACT_APP_VERIFIERS || 'DEV',
-    onboardingClientRepo: process.env.REACT_ONBOARDING_REPO ||'filecoin-clients-onboarding-test',
+    onboardingClientRepo: process.env.REACT_ONBOARDING_REPO || 'filecoin-clients-onboarding-test',
     githubApp: process.env.REACT_APP_GITHUB_APP || 'Iv1.10e7aaed4654db3c',
     lotusNodes: [{
-        name: 'Mainnet',
-        code: 461,
-        url: 'https://api.node.glif.io',
-        token: process.env.REACT_APP_MAINNET_TOKEN,
-        clientRepo: 'filecoin-clients-onboarding',
-        notaryRepo: 'filecoin-notaries-onboarding'
-    },{
-        name: 'Nerpanet',
-        code: 1,
-        url: 'https://beta-verify.filecoin.io/api/rpc/v0',
-        token: process.env.REACT_APP_NERPANET_TOKEN,
-        clientRepo: 'filecoin-clients-onboarding-test',
-        notaryRepo: 'filecoin-notaries-onboarding'
-    }/*,{
-        name: 'Localhost',
-        code: 1,
-        url: 'ws://localhost:1234/rpc/v0',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.OJlFIgYG3D23RjWWXfjdTluG6Qx2EOgwMeWQxnUQrMM'
-        clientRepo: 'filecoin-clients-onboarding-test',
-        notaryRepo: 'filecoin-notaries-onboarding',
-    }*/],
+            name: 'Mainnet',
+            code: 461,
+            url: 'https://api.node.glif.io',
+            token: process.env.REACT_APP_MAINNET_TOKEN,
+            clientRepo: 'filecoin-clients-onboarding',
+            notaryRepo: 'filecoin-notaries-onboarding'
+        }, {
+            name: 'Nerpanet',
+            code: 1,
+            url: 'https://beta-verify.filecoin.io/api/rpc/v0',
+            token: process.env.REACT_APP_NERPANET_TOKEN,
+            clientRepo: 'filecoin-clients-onboarding-test',
+            notaryRepo: 'filecoin-notaries-onboarding'
+        } /*,{
+            name: 'Localhost',
+            code: 1,
+            url: 'ws://localhost:1234/rpc/v0',
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.OJlFIgYG3D23RjWWXfjdTluG6Qx2EOgwMeWQxnUQrMM'
+            clientRepo: 'filecoin-clients-onboarding-test',
+            notaryRepo: 'filecoin-notaries-onboarding',
+        }*/
+    ],
     datacapExt: [
         { value: "1", name: "B" },
         { value: "1000", name: "KiB" },
@@ -50,4 +54,4 @@ export const config = {
     ],
     dataSource: process.env.REACT_APP_VERIFIES_DATA || 'verifiers-dev',
     rkhtreshold: 1
-}
+};
