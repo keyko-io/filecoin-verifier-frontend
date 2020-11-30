3334067d54e3b46ce232da60de2e9589ef9b037a
Iván <ivan.alberquilla@gmail.com>
2020-11-30 11:53:47 +0100

    api call verify

--- src/modals/AddClientModal.tsx
+++ src/modals/AddClientModal.tsx
@@ -53,7 +53,7 @@ class AddClientModal extends Component<ModalProps, States> {
 
             const datacap = parseFloat(this.state.datacap)
             const fullDatacap = BigInt(datacap * parseFloat(this.state.datacapExt))
-            let messageID = await this.context.api.verifyClient(this.state.address, fullDatacap, this.context.walletIndex);
+            let messageID = await this.context.wallet.api.verifyClient(this.state.address, fullDatacap, this.context.walletIndex);
            
             if (this.props.newDatacap) {
                 this.context.updateGithubVerified(this.state.issueNumber, messageID, this.state.address, fullDatacap)
