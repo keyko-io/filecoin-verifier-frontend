4b5708f568f6337b8c64ad691c3f9869264895b3
Iván <ivan.alberquilla@gmail.com>
2021-01-04 11:44:41 +0100

    dependencies & remove host detect

--- src/utils/HostDetect.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "../config";
-
-export function selectGitubApp() {
-    return window.location.host.includes('fleek') ?
-        { app: config.githubAppSecondary, uri: config.oauthUriSecondary } :
-        { app: config.githubApp, uri: config.oauthUri }
-}
