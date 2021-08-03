150dff49c03f4ce6c205922e003c8e6245919892
fabrizio <fabrizio@keyko.io>
2021-08-03 17:43:55 +0200

    miners table sorting and filtering

--- src/utils/Filters.ts
+++ src/utils/Filters.ts
@@ -1,7 +1,11 @@
 import ByteConverter from '@wtfcode/byte-converter'
 const byteConverter = new ByteConverter()
 
-export function addressFilter (input: string) {
+const filMultip = 1000000000000000000 //10^18
+const nanoMultip = 1000000000 //10^9
+const attoMultip = 1000 //10^3
+
+export function addressFilter(input: string) {
     return `${input.substr(0, 5)}...${input.substr(-5, 5)}`
 }
 
@@ -20,4 +24,51 @@ export function bytesToiB(inputBytes: any) {
 export function bytesToB(inputBytes: any) {
     const autoscale = byteConverter.autoScale(Number(inputBytes), 'B', { preferByte: true, preferDecimal: true } as any)
     return `${Number.isInteger(autoscale.value) ? autoscale.value : autoscale.value.toFixed(1)}${autoscale.dataFormat}`
+}
+
+export function anyToFil(val: string): string {
+    if (val === "not found") {
+        return val
+    }
+    const n = Number(val);
+    let retVal = ''
+    if (val === "0") {
+        retVal = '0 FIL'
+    }
+    if (val.length > 18) {
+        retVal = `${n / filMultip} FIL`
+    }
+    if (val.length <= 18 && val.length > 6) {
+        retVal = `${n / nanoMultip} nanoFIL`
+    }
+    if (val.length <= 6) {
+        retVal = `${n / attoMultip} attoFIL`
+    }
+    return retVal
+}
+
+export function filToAny(val: string): number {
+    if (val === "not found") {
+        console.log(val)
+        return -1
+    }
+    const num = Number(val.slice(0, val.indexOf(" ")))
+    let retVal = 0;
+
+    if (val === "0") {
+        retVal = 0
+    }
+
+    if (val.slice(val.indexOf(" ") + 1) === "FIL") {
+        retVal = num * filMultip
+    }
+
+    if (val.slice(val.indexOf(" ") + 1) === "nanoFIL") {
+        retVal = num * nanoMultip
+    }
+
+    if (val.slice(val.indexOf(" ") + 1) === "attoFIL") {
+        retVal = num * attoMultip
+    }
+    return retVal
 }
--- src/utils/SortFilter.ts
+++ src/utils/SortFilter.ts
@@ -1,8 +1,8 @@
+import { filToAny } from '../utils/Filters';
 
 export const tableSort = (e: any, arrayToSort: any[], previousOrderBy: string, previousSortOrder: number) => {
     const orderBy = e.currentTarget.id
     const sortOrder = orderBy === previousOrderBy ? previousSortOrder * -1 : -1
-
     return arrayToSort[0] && arrayToSort[0].data ?
         sortTableObject(arrayToSort, orderBy, sortOrder)
         :
@@ -10,10 +10,38 @@ export const tableSort = (e: any, arrayToSort: any[], previousOrderBy: string, p
 
 }
 
+export const tableSortMiners = (e: any, arrayToSort: any[], previousOrderBy: string, previousSortOrder: number) => {
+    const orderBy = e.currentTarget.id
+    const sortOrder = orderBy === previousOrderBy ? previousSortOrder * -1 : -1
+    return sortTableMiners(arrayToSort, orderBy, sortOrder)
+
+}
+
+const sortTableMiners = (arrayToSort: any[], orderBy: string, sortOrder: number) => {
+    const arraySorted = arrayToSort.sort((a: any, b: any) => {
+        if (orderBy === "verifiedPrice") {
+            return filToAny(a[orderBy]) < filToAny(b[orderBy]) ?
+                sortOrder :
+                filToAny(a[orderBy]) > filToAny(b[orderBy]) ?
+                    sortOrder * -1 : 0;
+        }
+        if (orderBy === "minPieceSize") {
+            return a["minPieceSizeRaw"] > b["minPieceSizeRaw"] ?
+                sortOrder :
+                a["minPieceSizeRaw"] < b["minPieceSizeRaw"] ?
+                    sortOrder * -1 : 0;
+        }
+
+        return a[orderBy] < b[orderBy] ?
+            sortOrder :
+            a[orderBy] > b[orderBy] ?
+                sortOrder * -1 : 0;
+    });
+
+    return { arraySorted, orderBy, sortOrder }
+}
 
 const sortTable = (arrayToSort: any[], orderBy: string, sortOrder: number) => {
-    console.log(orderBy)
-    console.log(sortOrder)
     const arraySorted = arrayToSort.sort((a: any, b: any) => {
         return a[orderBy] < b[orderBy] ?
             sortOrder :
@@ -42,6 +70,19 @@ export const tableFilter = async (search: string, tableData: []) => {
             return k === undefined ? false :
                 typeof (k) === 'object' ?
                     k.join().toLowerCase().includes(search.toLowerCase())
+                    :
+                    k.toString().toLowerCase().includes(search.toLowerCase())
+
+        });
+    })
+}
+
+export const tableMinerFilter = async (search: string, tableData: []) => {
+    return tableData.filter((element: any) => {
+        return Object.values(element).some((k: any) => {
+            return k === undefined ? false :
+                typeof (k) === 'object' ?
+                    Object.values(k).join().toLowerCase().includes(search.toLowerCase())
                     :
                     k.toString().toLowerCase().includes(search.toLowerCase())
 
