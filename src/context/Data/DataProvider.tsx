92b4cec04864a002014332d7064d964f5fcaa878
Fabriziogianni7 <fabrizio@keyko.io>
2021-12-22 17:29:38 +0100

    implemented api key to post logs

--- src/context/Data/DataProvider.tsx
+++ src/context/Data/DataProvider.tsx
@@ -67,9 +67,10 @@ export default class DataProvider extends React.Component<DataProviderProps, Dat
         this.state = {
             postLogs: async (message: string, type: string, actionKeyword: string, issueNumber: number) => {
                 try {
-                    const logArray = [{ message, type, actionKeyword, repo: "Fil+app", issueNumber:issueNumber.toString() }]
+                    const logArray = [{ message, type, actionKeyword, repo: "Fil+app", issueNumber: issueNumber.toString() }]
                     const res = (await fetch("https://cbqluey8wa.execute-api.us-east-1.amazonaws.com/dev",
                         {
+                            headers: { "x-api-key": config.loggerApiKey },
                             method: 'POST',
                             body: JSON.stringify({
                                 "type": "POST_CUSTOM_LOGS",
@@ -85,7 +86,7 @@ export default class DataProvider extends React.Component<DataProviderProps, Dat
                 try {
                     const res = (await fetch("https://cbqluey8wa.execute-api.us-east-1.amazonaws.com/dev",
                         {
-                            headers: {"x-api-key": config.loggerApiKey},
+                            headers: { "x-api-key": config.loggerApiKey },
                             method: 'POST',
                             body: JSON.stringify({
                                 "type": "GET_LOGS",
@@ -94,7 +95,7 @@ export default class DataProvider extends React.Component<DataProviderProps, Dat
                                 "search": issue_number
                             })
                         })).json()
-                        console.log("response log", res)
+                    console.log("response log", res)
                     return res
                 } catch (error) {
                     console.log(error)
