690bd2572da9e7b42c0c777e218e8cbac5008375
huseyin <huseyin@keyko.io>
2022-05-24 17:47:58 +0300

    fixed issue on the wallet accounts

--- src/context/Wallet/LedgerWallet.tsx
+++ src/context/Wallet/LedgerWallet.tsx
@@ -10,130 +10,132 @@ const VerifyAPI = require('@keyko-io/filecoin-verifier-tools/api/api')
 
 export class LedgerWallet {
 
-    ledgerBusy: boolean = false
-    ledgerApp: any = false
-    api: any
-    lotusNode: any
-    networkIndex: number = 0
+  ledgerBusy: boolean = false
+  ledgerApp: any = false
+  api: any
+  lotusNode: any
+  networkIndex: number = 0
 
-    public loadWallet = async(networkIndex: number) => {
-      this.networkIndex = networkIndex
-      this.lotusNode = config.lotusNodes[networkIndex]
-      this.api = new VerifyAPI(
-        VerifyAPI.browserProvider(this.lotusNode.url
-            , {
-            token: async () => {
-                return this.lotusNode.token
-            }})
-            , {sign: this.sign, getAccounts: this.getAccounts}
-            , this.lotusNode.name !== "Mainnet" // if node != Mainnet => testnet = true
-        )
-
-
-      let transport
-      try {
-          transport = await TransportWebUSB.create();
-      } catch (e) {
-          console.log('TransportWebUSB error', e)
-      }
-      if (transport) {
-          try {
-              this.ledgerApp = new FilecoinApp(transport);
-              const version = await this.ledgerApp.getVersion()
-              if(version.device_locked){
-                throw new Error('Ledger locked.')
-              }
-              if(version.test_mode){
-                throw new Error('Filecoin app in test mode.')
-              }
-              if(version.minor < 18){
-                throw new Error('Please update Filecoin app on Ledger.')
-              }
-              if(version.minor < 18 && version.patch < 2){
-                throw new Error('Please update Filecoin app on Ledger.')
-              }
-          } catch (e) {
-            throw new Error(e.message)
+  public loadWallet = async (networkIndex: number) => {
+    this.networkIndex = networkIndex
+    this.lotusNode = config.lotusNodes[networkIndex]
+    this.api = new VerifyAPI(
+      VerifyAPI.browserProvider(this.lotusNode.url
+        , {
+          token: async () => {
+            return this.lotusNode.token
           }
-      } else {
-          console.log('device not found')
-      }
-    }
-
-    public selectNetwork = async(nodeIndex: any) => {
-      this.lotusNode = config.lotusNodes[nodeIndex]
-      this.loadWallet(this.networkIndex)
-      return this
-    }
-
-    public getAccounts = async (nStart = 0, nEnd = 5) => {
-        const paths = []
-        for (let i = nStart; i < nEnd; i += 1) {
-          paths.push(`m/44'/${this.lotusNode.code}'/0'/0/${i}`)
-        }
-        const accounts = await mapSeries(paths, async path => {
-            const returnLoad = await this.ledgerApp.getAddressAndPubKey(path)
-            const { addrString } = this.handleErrors(returnLoad)
-            return addrString
         })
-        return accounts
+      , { sign: this.sign, getAccounts: this.getAccounts }
+      , this.lotusNode.name !== "Mainnet" // if node != Mainnet => testnet = true
+    )
+
+
+    let transport
+    try {
+      transport = await TransportWebUSB.create();
+    } catch (e) {
+      console.log('TransportWebUSB error', e)
     }
-
-    public sign = async (filecoinMessage:any, indexAccount:number) => {
-        const serializedMessage = signer.transactionSerialize(
-          filecoinMessage
-        )
-        const signedMessage = this.handleErrors(
-          await this.ledgerApp.sign(`m/44'/${this.lotusNode.code}'/0'/0/${indexAccount}`, Buffer.from(serializedMessage, 'hex'))
-        )
-      
-        return await this.generateSignedMessage(filecoinMessage, signedMessage)
-    }
-
-
-    private generateSignedMessage =  async (filecoinMessage:any, signedMessage:any) =>{
-
-      return JSON.stringify({
-        Message: {
-          From: filecoinMessage.from,
-          GasLimit: filecoinMessage.gaslimit,
-          GasFeeCap: filecoinMessage.gasfeecap,
-          GasPremium: filecoinMessage.gaspremium,
-          Method: filecoinMessage.method,
-          Nonce: filecoinMessage.nonce,
-          Params: Buffer.from(filecoinMessage.params, "hex").toString(
-            "base64"
-          ),
-          To: filecoinMessage.to,
-          Value: filecoinMessage.value,
-        },
-        Signature: {
-          Data: signedMessage.signature_compact.toString('base64'),
-          Type: 1
-          //Type: signedMessage.signature.type,
-        },
-      });
-
-
-    }
-
-    private handleErrors = (response:any) => {
-        if (
-          response.error_message &&
-          response.error_message.toLowerCase().includes('no errors')
-        ) {
-          return response
+    if (transport) {
+      try {
+        this.ledgerApp = new FilecoinApp(transport);
+        const version = await this.ledgerApp.getVersion()
+        if (version.device_locked) {
+          throw new Error('Ledger locked.')
         }
-        if (
-          response.error_message &&
-          response.error_message
-            .toLowerCase()
-            .includes('transporterror: invalild channel')
-        ) {
-          throw new Error(
-            'Lost connection with Ledger. Please unplug and replug device.'
-          )
+        if (version.test_mode) {
+          throw new Error('Filecoin app in test mode.')
         }
-        throw new Error(response.error_message)
+        if (version.minor < 18) {
+          throw new Error('Please update Filecoin app on Ledger.')
+        }
+        if (version.minor < 18 && version.patch < 2) {
+          throw new Error('Please update Filecoin app on Ledger.')
+        }
+      } catch (e) {
+        throw new Error(e.message)
+      }
+    } else {
+      console.log('device not found')
     }
+  }
+
+  public selectNetwork = async (nodeIndex: any) => {
+    this.lotusNode = config.lotusNodes[nodeIndex]
+    this.loadWallet(this.networkIndex)
+    return this
+  }
+
+  public getAccounts = async (nStart = 0, nEnd = 15) => {
+    const paths = []
+    for (let i = nStart; i < nEnd; i += 1) {
+      paths.push(`m/44'/${this.lotusNode.code}'/0'/0/${i}`)
+    }
+    const accounts = await mapSeries(paths, async path => {
+      const returnLoad = await this.ledgerApp.getAddressAndPubKey(path)
+      const { addrString } = this.handleErrors(returnLoad)
+      return addrString
+    })
+
+    return accounts
+  }
+
+  public sign = async (filecoinMessage: any, indexAccount: number) => {
+    const serializedMessage = signer.transactionSerialize(
+      filecoinMessage
+    )
+    const signedMessage = this.handleErrors(
+      await this.ledgerApp.sign(`m/44'/${this.lotusNode.code}'/0'/0/${indexAccount}`, Buffer.from(serializedMessage, 'hex'))
+    )
+
+    return await this.generateSignedMessage(filecoinMessage, signedMessage)
+  }
+
+
+  private generateSignedMessage = async (filecoinMessage: any, signedMessage: any) => {
+
+    return JSON.stringify({
+      Message: {
+        From: filecoinMessage.from,
+        GasLimit: filecoinMessage.gaslimit,
+        GasFeeCap: filecoinMessage.gasfeecap,
+        GasPremium: filecoinMessage.gaspremium,
+        Method: filecoinMessage.method,
+        Nonce: filecoinMessage.nonce,
+        Params: Buffer.from(filecoinMessage.params, "hex").toString(
+          "base64"
+        ),
+        To: filecoinMessage.to,
+        Value: filecoinMessage.value,
+      },
+      Signature: {
+        Data: signedMessage.signature_compact.toString('base64'),
+        Type: 1
+        //Type: signedMessage.signature.type,
+      },
+    });
+
+
+  }
+
+  private handleErrors = (response: any) => {
+    if (
+      response.error_message &&
+      response.error_message.toLowerCase().includes('no errors')
+    ) {
+      return response
+    }
+    if (
+      response.error_message &&
+      response.error_message
+        .toLowerCase()
+        .includes('transporterror: invalild channel')
+    ) {
+      throw new Error(
+        'Lost connection with Ledger. Please unplug and replug device.'
+      )
+    }
+    throw new Error(response.error_message)
+  }
 }
