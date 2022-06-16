63a8146d62bfaad7474335d94683772f88535fb6
huseyin <huseyin@keyko.io>
2022-06-16 12:53:03 +0300

    small fixes

--- src/pages/tableUtils/verifiersColumns.tsx
+++ src/pages/tableUtils/verifiersColumns.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { Verifier } from "../VerifiersData/index";
+
 
 export const columns: any = [
   {
     name: "Notary Name",
-    selector: (row: Verifier) => row.name,
+    selector: (row: any) => row.name,
     sortable: true,
   },
   {
     name: "Use Case",
-    selector: (row: Verifier) => row.use_case,
+    selector: (row: any) => row.use_case,
     sortable: true,
-    cell: (row: Verifier) => <span>{row.use_case.join(", ")}</span>,
+    cell: (row: any) => <span>{row.use_case.join(", ")}</span>,
   },
   {
     name: "Location",
-    selector: (row: Verifier) => row.location,
+    selector: (row: any) => row.location,
     sortable: true,
   },
   {
     name: "Contacts",
-    selector: (row: Verifier) => [row.fil_slack_id, row.github_user],
+    selector: (row: any) => [row.fil_slack_id, row.github_user],
     sortable: false,
-    cell: (row: Verifier) => (
+    cell: (row: any) => (
       <div style={{ display: "flex", flexDirection: "column" }}>
         <span>Slack : {row.fil_slack_id}</span>
         <span>Github : {row.github_user}</span>
