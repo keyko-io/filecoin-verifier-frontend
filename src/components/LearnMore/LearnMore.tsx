2261cfd6459bdfb609135df8844cafd3eeb29b07
huseyin <huseyin@keyko.io>
2022-07-05 17:31:45 +0300

    move logs

--- src/components/LearnMore/LearnMore.tsx
+++ src/components/LearnMore/LearnMore.tsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 // @ts-ignore
 import { ButtonPrimary } from "slate-react-system";
 import "./LearnMore.scss"
+import history from "../../context/History";
 
 const LearnMore = () => {
     let { pathname } = useLocation();
@@ -11,14 +12,20 @@ const LearnMore = () => {
         window.open('https://verify.glif.io/', '_blank')
     }
 
+    const goToLogs = () => {
+        history.push({
+            pathname: "/logs"
+        })
+    }
+
     const navigate = () => {
         window.open('https://docs.filecoin.io/store/filecoin-plus/', '_blank')
     }
 
     return (
         <div className="learnmore">
-            {pathname !== "/" && <ButtonPrimary style={{ marginRight: "30px" }} onClick={checkDatacap}>Check Remaining DataCap</ButtonPrimary>}
-            <ButtonPrimary style={{ marginLeft: pathname === "/" ? "" : "30px" }} onClick={navigate}>Learn More</ButtonPrimary>
+            {pathname !== "/" ? <ButtonPrimary style={{ marginRight: "30px" }} onClick={checkDatacap}>Check Remaining DataCap</ButtonPrimary> : <ButtonPrimary style={{ marginRight: "30px" }} onClick={goToLogs}>See the logs</ButtonPrimary>}
+            <ButtonPrimary style={{ marginLeft: "30px" }} onClick={navigate}>Learn More</ButtonPrimary>
         </div >
     )
 
