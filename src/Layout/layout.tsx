0f808233e873bd87102c942608db668c49ff33d3
huseyin <huseyin@keyko.io>
2022-09-28 18:40:04 +0300

    added react toaster

--- src/Layout/layout.tsx
+++ src/Layout/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router-dom';
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
+import { Toaster } from 'react-hot-toast';
 
 
 const Layout = ({ children }: any) => {
@@ -11,6 +12,9 @@ const Layout = ({ children }: any) => {
     let { pathname } = useLocation();
 
     return (<>
+        <Toaster position="top-right" containerStyle={{
+            top: 80,
+        }} />
         {!excludeHeaderPaths.includes(pathname) && <Header />}
         {children}
         <Footer />
