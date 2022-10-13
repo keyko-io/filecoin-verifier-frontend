f635dd665ddaab5ca8e974f364920304f9fe8c82
hcsoylu <hcsoylu.dev@gmail.com>
2022-10-13 23:11:16 +0300

    added new client details table

--- src/pages/ClientDetails.tsx
+++ src/pages/ClientDetails.tsx
@@ -1,60 +1,84 @@
-import React, { Component } from 'react';
-// @ts-ignore
-import WelcomeClient from '../components/WelcomeClient'
-import { Data } from '../context/Data/Index'
-import TableClientDetails from '../components/TableClientDetails';
+import React, { useContext, useEffect, useState } from 'react';
 import history from '../context/History'
+import DataTable from 'react-data-table-component';
+import { Data } from '../context/Data/Index';
 
-type ClientDetailsStates = {
-  name: string
+type ClientInfoStateType = {
+  client: string
   user: string
   address: string
   datacap: string
 }
 
+const clientDetailsColumns: any = [
+  {
+    name: "Address",
+    selector: (row: any) => row.data.address,
+  },
+  {
+    name: "Notary",
+    selector: (row: any) => row.data.notary,
+  },
+  {
+    name: "Datacap",
+    selector: (row: any) => row.data.datacap,
+  },
+  {
+    name: "State",
+    selector: (row: any) => row.state,
+  },
+  {
+    name: "Link",
+    selector: (row: any) => row.number,
+  },
+  {
+    name: "Date",
+    selector: (row: any) => row.created_at,
+    cell: (row: any) => <span>{row.created_at.split('T')[0]}</span>
+  },
+]
 
-class ClientDetails extends Component<{}, ClientDetailsStates> {
+const ClientDetails = () => {
+  const context = useContext(Data)
+  const [details, setDetails] = useState<any>([])
+  const [clientInfo, setClientInfo] = useState<ClientInfoStateType>({
+    client: "",
+    user: "",
+    address: "",
+    datacap: "",
+  })
 
-  public static contextType = Data
-
-  child: any
-
-  constructor(props: {}) {
-    super(props);
-    this.child = React.createRef();
-    this.state = {
-      name: "",
-      user: "",
-      address: "",
-      datacap: ""
+  useEffect(() => {
+    if (!history.location.state) {
+      history.push("/")
     }
+
+    setClientInfo(history.location.state as ClientInfoStateType)
+    loadDetails()
+  }, [])
+
+  const loadDetails = async () => {
+    const clientDetailsData = await context.searchUserIssues(clientInfo.user)
+    setDetails(clientDetailsData)
   }
 
-  componentDidMount() {
-    const params = history.location.state as any
-    const name = params.client
-    const user = params.user
-    const address = params.address
-    const datacap = params.datacap
+  return (
+    <div style={{ maxWidth: "1280px", margin: "auto", width: "100%", marginTop: "4.4rem" }}>
+      <div>Heyyo</div>
+      <DataTable
+        columns={clientDetailsColumns}
+        data={details}
+        pagination
+        paginationRowsPerPageOptions={[10, 20, 30]}
+        paginationPerPage={10}
+        noDataComponent="No client requests yet"
+      />
+    </div>
+  );
 
-
-    this.setState({ name, user, address, datacap })
-  }
-
-  render() {
-    return (
-      <div className="landing">
-        <div className="container">
-          <WelcomeClient client={this.state.name} user={this.state.user} address={this.state.address} datacap={this.state.datacap} />
-          <div className="wrapperverifiers wrapperclients">
-            <div className="tableselects" style={this.context.github.githubLogged === false ? { zIndex: -1 } : {}}>
-            </div>
-            <TableClientDetails user={this.state.user} />
-          </div>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default ClientDetails;
+
+
+
