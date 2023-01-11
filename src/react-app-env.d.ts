62165967f42229e2a5982edddca25b0b030246d0
Fabriziogianni7 <fabrizio@keyko.io>
2023-01-11 13:12:11 +0100

    other

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="react-scripts" />
-declare module '@keyko-io/filecoin-verifier-tools/*';
 declare module '@zondax/filecoin-signing-tools/js';
 declare module '@zondax/ledger-filecoin';
+declare module '@keyko-io/filecoin-verifier-tools'
