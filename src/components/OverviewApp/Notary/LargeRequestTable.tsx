97b638f4a810d14d1b31087111d3332907d3b815
hcsoylu <hcsoylu.dev@gmail.com>
2023-01-23 16:30:19 +0300

    filter data logic

--- src/components/OverviewApp/Notary/LargeRequestTable.tsx
+++ src/components/OverviewApp/Notary/LargeRequestTable.tsx
@@ -1,13 +1,16 @@
 import { CircularProgress } from "@material-ui/core"
-import React, { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import DataTable from "react-data-table-component"
 import { Data } from "../../../context/Data/Index"
+import Tooltip from '@mui/material/Tooltip';
 
 export const largeReqColumns: any = [
     {
         name: "Client",
         selector: (row: any) => row.data,
         sortable: true,
+        grow: 1.2,
+        wrap: true
     },
     {
         name: "Address",
@@ -19,19 +22,19 @@ export const largeReqColumns: any = [
         name: "Multisig",
         selector: (row: any) => row.multisig,
         sortable: true,
-        grow: 0.6
+        grow: 0.5
     },
     {
         name: "Datacap",
         selector: (row: any) => row.datacap,
         sortable: true,
-        grow: 0.6
+        grow: 0.5
     },
     {
         name: "Audit Trail",
         selector: (row: any) => row.issue_number,
         sortable: true,
-        grow: 0.6,
+        grow: 0.5,
         cell: (row: any) => <a
             target="_blank"
             rel="noopener noreferrer"
@@ -61,16 +64,36 @@ type LargeRequestTableProps = {
     setSelectedLargeClientRequests: any
     dataForLargeRequestTable: any
     largeRequestListLoading: boolean
+    setDataForLargeRequestTable: any
 }
 
-const LargeRequestTable = ({ setSelectedLargeClientRequests, largeRequestListLoading, dataForLargeRequestTable }: LargeRequestTableProps) => {
+const LargeRequestTable = ({ setSelectedLargeClientRequests, largeRequestListLoading, dataForLargeRequestTable, setDataForLargeRequestTable }: LargeRequestTableProps) => {
 
     const context = useContext(Data)
 
     const CANT_SIGN_MESSAGE = "You can currently only approve the allocation requests associated with the multisig organization you signed in with. Signing proposals for additional DataCap allocations will require you to sign in again";
 
+    const [searched, setSearched] = useState(false)
+
+    useEffect(() => {
+        if (context.searchString) {
+            setDataForLargeRequestTable((prev: any) => prev.filter((item: any) => item.searchBy.toLowerCase().includes(context.searchString.toLowerCase())))
+            setSearched(true)
+        }
+        if (context.searchString === "" && context.largeClientRequests) {
+            const data = context.largeClientRequests
+                .map((item: any) => ({ ...item, data: item.data.name, searchBy: `${item.data.name} ${item.issue_number} ${item.address}` }))
+                .map((item: any) => item.tx !== null ? item : { ...item, tx: "", })
+            setDataForLargeRequestTable(data)
+        }
+    }, [context.searchString, context.largeClientRequests])
+
+
     return (
         <div className="large-request-table" style={{ minHeight: "500px" }}>
+            {!context.ldnRequestsLoading && <p style={{ margin: "1rem 2rem" }}>
+                * <i>You can look for specific deals querying by "Client", "Address" and "Audit Trail" fields using the search box.</i>
+            </p>}
             {context.ldnRequestsLoading ?
                 <div style={{ width: "100%" }} >
                     <CircularProgress style={{ margin: "8rem auto", color: "#0090ff" }} />
@@ -86,7 +109,7 @@ const LargeRequestTable = ({ setSelectedLargeClientRequests, largeRequestListLoa
                     paginationRowsPerPageOptions={[10, 20, 30]}
                     paginationPerPage={10}
                     defaultSortFieldId={1}
-                    noDataComponent="No large client requests yet"
+                    noDataComponent={searched ? "We could not find the data you are looking for" : "No large client requests yet"}
                     onSelectedRowsChange={({ selectedRows }) => {
                         const rowNumbers = selectedRows.map((row: any) => row.issue_number)
                         setSelectedLargeClientRequests(rowNumbers)
