55f4986ebd07e98d870adb5e78cefbea37090ac3
hcsoylu <hcsoylu.dev@gmail.com>
2023-01-26 17:17:48 +0300

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,12 @@ import SearchIcon from '@mui/icons-material/Search';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, far, fas)
 
 const App = () => {
   const context: any = useContext(Data)
