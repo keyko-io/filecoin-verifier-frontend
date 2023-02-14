456fc05c7bbd4e9a9f592db6d81c826ae63ac343
esraa <jbesraa@gmail.com>
2023-02-14 13:28:08 +0200

    fix conflict

--- src/context/Github/Index.tsx
+++ src/context/Github/Index.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export const Github = React.createContext({
-    loginGithub: async (code: string) => { },
-    initGithubOcto: async (token: string) => { },
-    logoutGithub: async () => { },
-    githubOctoGenericLogin: async () => { },
-    fetchGithubIssues: async (owner: string, repo: string, state: any, labels: any) => { },
-    fetchGithubComments: async (owner: string, repo: string, issueNumber: number, issue?: any) => { },
+    loginGithub: async (code: string) => {},
     githubLogged: false,
-    loggedUser: '',
-    avatarUrl: '',
-    githubOctoGeneric: {},
     githubOcto: {},
-})
+    initGithubOcto: {} as any,
+    logoutGithub: async () => {},
+    githubOctoGenericLogin: async () => {},
+    loggedUser: "",
+    avatarUrl: "",
+    githubOctoGeneric: {},
+    fetchGithubIssues: async (
+        owner?: any,
+        repo?: any,
+        state?: any,
+        labels?: any
+    ) => {},
+    fetchGithubComments: async (
+        owner?: any,
+        repo?: any,
+        issueNumber?: any,
+        issue?: any
+    ) => {},
+});
