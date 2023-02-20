13721403633d0f84214b69f6d18841d541380b28
hcsoylu <hcsoylu.dev@gmail.com>
2023-02-20 14:55:29 +0300

    fix conflict

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { config } from "./config";
 import App from "./App";
@@ -15,21 +15,20 @@ import "./fonts/SuisseIntl-Regular.woff";
 import Layout from "../src/Layout/layout";
 import LdnApplication from "./pages/LdnApplication";
 
-import * as serviceWorker from './serviceWorker';
-import Preonboarding from './pages/Preonboarding';
-import Onboarding from './pages/Onboarding';
-import Landing from './pages/Landing';
-import history from './context/History';
-import LogExplorer from './pages/LogExplorer';
-import './fonts/SuisseIntl-Regular.woff'
-import ClientDetails from './pages/ClientDetails'
-import { CookiesProvider } from 'react-cookie';
+import * as serviceWorker from "./serviceWorker";
+import Preonboarding from "./pages/Preonboarding";
+import Onboarding from "./pages/Onboarding";
+import Landing from "./pages/Landing";
+import history from "./context/History";
+import LogExplorer from "./pages/LogExplorer";
+import "./fonts/SuisseIntl-Regular.woff";
+import ClientDetails from "./pages/ClientDetails";
+import { CookiesProvider } from "react-cookie";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import Verifiers from "./pages/Verifiers";
 import Miners from "./pages/Miners";
 import StatusPage from "./pages/StatusPage";
-
 
 // redirect to domain if user access fleek url
 if (window.location.host.includes("fleek") && config.willRedirect) {
@@ -46,7 +45,9 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <>
@@ -61,7 +62,11 @@ root.render(
                     <Router history={history}>
                       <Layout>
                         <Switch>
-                          <Route exact path={"/"} component={Onboarding}></Route>
+                          <Route
+                            exact
+                            path={"/"}
+                            component={Onboarding}
+                          ></Route>
                           <Route path={"/app"} component={App}></Route>
                           <Route
                             path={"/wallet"}
@@ -82,9 +87,12 @@ root.render(
                             component={ClientDetails}
                           ></Route>
                           <Route path={"/logs"} component={LogExplorer}></Route>
-                          <Route path={"/status"} component={StatusPage}></Route>
+                          <Route
+                            path={"/status"}
+                            component={StatusPage}
+                          ></Route>
                         </Switch>
-                      </Layout >
+                      </Layout>
                     </Router>
                     <GlobalNotification style={{ bottom: 0, right: 0 }} />
                     <GlobalModal style={{ maxWidth: "none" }} />
@@ -96,7 +104,7 @@ root.render(
         </GithubProvider>
       </WalletProvider>
     </CookiesProvider>
-  </>,
+  </>
 );
 
 // If you want your app to work offline and load faster, you can change
