edf52cdc3338ead855a7db79ecd8a21e002bad58
hcsoylu <hcsoylu.dev@gmail.com>
2023-03-15 18:19:55 +0300

    double propose check

--- src/components/OverviewApp/Overview.tsx
+++ src/components/OverviewApp/Overview.tsx
@@ -1,87 +1,100 @@
-import { useContext, useEffect, useState } from 'react';
-import { Data } from '../../context/Data/Index';
+import { useContext, useEffect, useState } from "react"
+import { Data } from "../../context/Data/Index"
 import { bytesToiB } from "../../utils/Filters"
 // @ts-ignore
-import RootKeyHolder from './RootKeyHolder';
-import Notary from './Notary';
-import { BeatLoader } from "react-spinners";
+import RootKeyHolder from "./RootKeyHolder"
+import Notary from "./Notary"
+import { BeatLoader } from "react-spinners"
 import "./Overview.scss"
-import { ApprovedVerifiers } from '../../type';
+import { ApprovedVerifiers } from "../../type"
+import { preventDoublePropose } from "../../utils/preventDoublePropose"
 
 const Overview = () => {
-    const context = useContext(Data)
+  const context = useContext(Data)
 
-    const [approvedNotariesLoading, setApprovedNotariesLoading] = useState(false)
-    const [approvedNotariesLength, setApprovedNotariesLength] = useState<number | null>(null)
+  const [approvedNotariesLoading, setApprovedNotariesLoading] = useState(false)
+  const [approvedNotariesLength, setApprovedNotariesLength] = useState<
+    number | null
+  >(null)
 
-    useEffect(() => {
-        context.loadClients()
+  useEffect(() => {
+    preventDoublePropose(context, 1482).then((data) => console.log(data))
 
-        setApprovedNotariesLoading(true)
+    context.loadClients()
 
-        context.wallet.api.listVerifiers().then((data: ApprovedVerifiers[]) => {
-            setApprovedNotariesLength(data.length)
-            setApprovedNotariesLoading(false)
-        })
-    }, [])
+    setApprovedNotariesLoading(true)
 
-    return (
-        <div className="page">
-            <div className="info">
-                <div className="textinfo">
-                    <div className="textinfotext">
-                        <h2>Welcome to the Filecoin Plus App</h2>
-                        <div className="description">Filecoin Plus is a layer of social trust on top of the Filecoin Network to help incentivize the storage of real data.</div>
-                    </div>
-                    <div className="textinfodata">
-                        <div className="textinfodatablock">
-                            <div className="data">{
-                                !context.clientsAmount ?
-                                    <div>
-                                        <span className="zeroOpaque">0B</span>
-                                        <BeatLoader size={15} color={"rgb(24,160,237)"} />
-                                    </div>
-                                    :
-                                    bytesToiB(Number(context.clientsAmount))}</div>
-                            <div className="text">Datacap Granted</div>
-                        </div>
-                        <div className="textinfodatablock">
-                            <div className="data">{
-                                context.isPendingRequestLoading ?
-                                    <div>
-                                        <span className="zeroOpaque">0</span>
-                                        <BeatLoader size={15} color={"rgb(24,160,237)"} />
-                                    </div>
-                                    :
-                                    context.verifierAndPendingRequests.length}</div>
-                            <div className="text">Pending Notaries</div>
-                        </div>
-                        <div className="textinfodatablock">
-                            <div className="data">{
-                                approvedNotariesLoading ?
-                                    <div>
-                                        <span className="zeroOpaque">0</span>
-                                        <BeatLoader size={15} color={"rgb(24,160,237)"} />
-                                    </div>
-                                    : approvedNotariesLength
+    context.wallet.api.listVerifiers().then((data: ApprovedVerifiers[]) => {
+      setApprovedNotariesLength(data.length)
+      setApprovedNotariesLoading(false)
+    })
+  }, [])
 
-                            }</div>
-                            <div className="text">Approved Notaries</div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="page">
+      <div className="info">
+        <div className="textinfo">
+          <div className="textinfotext">
+            <h2>Welcome to the Filecoin Plus App</h2>
+            <div className="description">
+              Filecoin Plus is a layer of social trust on top of the Filecoin
+              Network to help incentivize the storage of real data.
             </div>
-            {context.viewroot ?
-                <RootKeyHolder />
-                :
-                <Notary
-                    notaryProps={{
-                        clients: context.clients,
-                    }}
-                />
-            }
+          </div>
+          <div className="textinfodata">
+            <div className="textinfodatablock">
+              <div className="data">
+                {!context.clientsAmount ? (
+                  <div>
+                    <span className="zeroOpaque">0B</span>
+                    <BeatLoader size={15} color={"rgb(24,160,237)"} />
+                  </div>
+                ) : (
+                  bytesToiB(Number(context.clientsAmount))
+                )}
+              </div>
+              <div className="text">Datacap Granted</div>
+            </div>
+            <div className="textinfodatablock">
+              <div className="data">
+                {context.isPendingRequestLoading ? (
+                  <div>
+                    <span className="zeroOpaque">0</span>
+                    <BeatLoader size={15} color={"rgb(24,160,237)"} />
+                  </div>
+                ) : (
+                  context.verifierAndPendingRequests.length
+                )}
+              </div>
+              <div className="text">Pending Notaries</div>
+            </div>
+            <div className="textinfodatablock">
+              <div className="data">
+                {approvedNotariesLoading ? (
+                  <div>
+                    <span className="zeroOpaque">0</span>
+                    <BeatLoader size={15} color={"rgb(24,160,237)"} />
+                  </div>
+                ) : (
+                  approvedNotariesLength
+                )}
+              </div>
+              <div className="text">Approved Notaries</div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      {context.viewroot ? (
+        <RootKeyHolder />
+      ) : (
+        <Notary
+          notaryProps={{
+            clients: context.clients,
+          }}
+        />
+      )}
+    </div>
+  )
 }
 
 export default Overview
---
+++ src/utils/preventDoublePropose.ts
@@ -0,0 +1,42 @@
+import { ldnParser } from "@keyko-io/filecoin-verifier-tools"
+import { config } from "../config"
+
+export const preventDoublePropose = async (
+  context: any,
+  issueNumber: number
+) => {
+  const comments = await context.github.githubOcto.paginate(
+    context.github.githubOcto.issues.listComments,
+    {
+      owner: config.onboardingLargeOwner,
+      repo: config.onboardingLargeClientRepo,
+      issue_number: issueNumber,
+    }
+  )
+
+  const reversedComments = comments.reverse()
+
+  let proposeComment
+  let datacapCommment
+
+  for (let i = 0; i < reversedComments.length; i++) {
+    if (reversedComments[i].body.includes("Request Proposed")) {
+      proposeComment = reversedComments[i].body
+    }
+    if (reversedComments[i].body.includes("DataCap Allocation requested")) {
+      datacapCommment = reversedComments[i].body
+      break
+    }
+  }
+
+  const { uuid: datacapId } = ldnParser.parseReleaseRequest(datacapCommment)
+
+  const { uuid: proposeId } =
+    ldnParser.parseApprovedRequestWithSignerAddress(proposeComment)
+
+  if (datacapId === proposeId) {
+    return true
+  }
+
+  return false
+}
