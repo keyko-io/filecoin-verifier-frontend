e806a2dc173c4f9018e63c58d4a857a2faef5f7a
jbesraa <jbesraa@gmail.com>
2023-04-20 15:36:42 +0300

    update types

--- src/type.ts
+++ src/type.ts
@@ -17,17 +17,38 @@ export interface LargeRequestData {
     data: any;
     datacap: string;
     issue_number: number;
-    labels: any;
+    labels: { name: string }[];
     multisig: string;
     proposer: any;
     signable: boolean;
     tx: any;
     url: string;
-    comments: { user: { login: string }, body: string, created_at: string, updated_at: string }[];
+    comments: {
+        user: { login: string };
+        body: string;
+        created_at: string;
+        updated_at: string;
+    }[];
     approvalInfoFromLabels?: number;
     user: string; //github handle
+    events: GithubIssueEvent[];
 }
 
+export interface GithubIssueEvent {
+    id: number;
+    node_id: string;
+    url: string;
+    created_at: string;
+    label: {
+        name: string;
+        color: string;
+    };
+    actor: {
+        login: string;
+        id: number;
+    };
+    event: string; // "labeled" || "assigned" || "commented" || "closed" || "reopened" || "unassigned"
+}
 export interface DirectIssue {
     number: number;
     url: string;
